fe61089f6c0b49c79120ee03327f5f45cf4821ad
LENOVO-PC\Irina <polina_zelenaj@tut.by>
2018-04-03 22:23:01 +0300

    mostly fixed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-function sum() {
-    var a = document.getElementById('one').innerHTML.value;
-    var b = document.getElementById('two').innerHTML.value;
-    return a + b;
-}
-alert (sum());
-
-
-
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons = document.querySelectorAll('button');
+    buttons.forEach( function(buttons) {
+        buttons.addEventListener("click", counter, false);
+    function counter() {
+        calc.input.value += buttons.value;
+        calc.input.value = eval(calc.input.value);
+    }
+      });
+ });
